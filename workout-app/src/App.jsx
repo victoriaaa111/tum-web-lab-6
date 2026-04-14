2e9ab923b10ef0889fade1a8622a630fd167b23e
victoriaaa111 <victoria.mutruc@gmail.com>
2026-04-14 22:59:22 +0300

    Configure tailwind with color vars fo both dark and light theme  and google fonts. Add basic css

--- workout-app/src/App.jsx
+++ workout-app/src/App.jsx
@@ -1,10 +1,8 @@
-
 function App() {
-
   return (
-    <>
-      <h1 class="text-blue-600">Workout Journal</h1>
-    </>
+    <div className="min-h-screen font-body text-ink">
+      <h1 className="font-display text-4xl p-6">Workout Journal</h1>
+    </div>
   )
 }
 
